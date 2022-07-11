81e9ef4566086792eddcbcbfabd4587ca999662a
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-07-12 01:09:13 +0200

    feat: two sum hashmap, bruteforce and two pointer methods

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,17 +1,10 @@
-from abc import ABC, abstractmethod
 from typing import List
 
 import pytest
 
 
-class SolutionBase(ABC):
-    @abstractmethod
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
-
-
 # O(N^2)
-class BruteforceSolution(SolutionBase):
+class BruteforceSolution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
 
@@ -27,7 +20,7 @@ class BruteforceSolution(SolutionBase):
 
 
 # O(N)
-class HashMapSolution(SolutionBase):
+class HashMapSolution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
 
@@ -41,8 +34,33 @@ class HashMapSolution(SolutionBase):
         return []
 
 
-class Solution(HashMapSolution):
-    pass
+class TwoPointersSolution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums = sorted(nums)
+
+        for left in range(len(sorted_nums) - 1):
+            right = len(sorted_nums) - 1
+
+            while left < right:
+                left_value = sorted_nums[left]
+                right_value = sorted_nums[right]
+
+                temp_sum = left_value + right_value
+
+                if temp_sum > target:
+                    right -= 1
+                elif temp_sum < target:
+                    left += 1
+                else:
+                    return [nums.index(left_value),
+                            nums.index(right_value, left + 1)
+                            if left_value == right_value
+                            else nums.index(right_value)]
+
+        return []
+
+
+Solution = TwoPointersSolution
 
 
 @pytest.mark.parametrize(
@@ -50,6 +68,7 @@ class Solution(HashMapSolution):
     [
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
+        ([-10, -1, -18, -19], -19, [2, 1]),
         ([3, 3], 6, [0, 1]),
     ],
 )
