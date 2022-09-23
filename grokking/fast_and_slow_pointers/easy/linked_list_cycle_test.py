1f5c2816b30be565fec11a0df7954fc2ee6ef146
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-09-23 19:52:13 +0200

    feat(fast_slow_pointers): add additional task 1 problem

--- grokking/fast_and_slow_pointers/easy/linked_list_cycle_test.py
+++ grokking/fast_and_slow_pointers/easy/linked_list_cycle_test.py
@@ -25,6 +25,28 @@ def has_cycle(head):
     return False
 
 
+def find_cycle_length(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:  # found the cycle
+            return calculate_cycle_length(slow)
+
+    return 0
+
+
+def calculate_cycle_length(slow):
+    current = slow
+    cycle_length = 0
+    while True:
+        current = current.next
+        cycle_length += 1
+        if current == slow:
+            break
+    return cycle_length
+
+
 def test():
     head = Node(1)
     head.next = Node(2)
@@ -39,3 +61,17 @@ def test():
 
     head.next.next.next.next.next.next = head.next.next.next
     assert has_cycle(head)
+
+
+def test_simmilar_problem():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = head.next.next
+    assert find_cycle_length(head) == 4
+
+    head.next.next.next.next.next.next = head.next.next.next
+    assert find_cycle_length(head) == 3
