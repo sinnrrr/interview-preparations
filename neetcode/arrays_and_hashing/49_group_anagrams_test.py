d212693091ba4cae85145204a9045dfaac0c4aa2
Dmytro Soltysiuk <dimasoltusyuk@gmail.com>
2023-11-12 17:21:32 -0800

    feat(nc): 49 done

--- neetcode/arrays_and_hashing/49_group_anagrams_test.py
+++ neetcode/arrays_and_hashing/49_group_anagrams_test.py
@@ -1,3 +1,30 @@
+from collections import defaultdict
+
+import pytest
+
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        known_anagrams = {}
+        hmap = defaultdict(list[str])
+        a_ord = ord("a")
+        for s1 in strs:
+            fingerprint = [0] * 26
+            for char in s1:
+                fingerprint[ord(char) - a_ord] += 1
+            hmap[tuple(fingerprint)].append(s1)
+        return hmap.values()
+
+
+@pytest.mark.parametrize(
+    "strs, expected",
+    [
+        (
+            ["eat", "tea", "tan", "ate", "nat", "bat"],
+            [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]],
+        ),
+        ([""], [[""]]),
+        (["a"], [["a"]]),
+    ],
+)
+def test_solution(strs, expected):
+    assert Solution().groupAnagrams(strs) == expected
