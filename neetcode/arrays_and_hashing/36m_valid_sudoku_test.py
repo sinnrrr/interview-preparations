e7c1b505c18a1f31df8f96333ffe07f04957f61a
Dmytro Soltysiuk <dimasoltusyuk@gmail.com>
2024-10-24 21:43:26 -0700

    wow, spent 30 mins trying to figure out which subbox does the index belongs, when in fact it's row // 3, col // 3, thanks.

--- neetcode/arrays_and_hashing/36m_valid_sudoku_test.py
+++ neetcode/arrays_and_hashing/36m_valid_sudoku_test.py
@@ -1,3 +1,52 @@
+from collections import defaultdict
+
+
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        pass
+        rows = [defaultdict(int) for _ in range(9)]
+        cols = [defaultdict(int) for _ in range(9)]
+        boxes = [[defaultdict(int) for _ in range(3)] for _ in range(3)]
+
+        for i in range(9 * 9):
+            row, col = divmod(i, 9)
+            curr_numstr = board[row][col]
+            if curr_numstr == ".":
+                continue
+            curr_num = int(curr_numstr)
+
+            if curr_num < 1 or curr_num > 9:
+                return False
+
+            rows[row][curr_num] += 1
+            if rows[row][curr_num] > 1:
+                return False
+
+            cols[col][curr_num] += 1
+            if cols[col][curr_num] > 1:
+                return False
+
+            box_row, box_col = row // 3, col // 3
+            boxes[box_row][box_col][curr_num] += 1
+            if boxes[box_row][box_col][curr_num] > 1:
+                return False
+
+        return True
+
+
+def test():
+    assert (
+        Solution().isValidSudoku(
+            [
+                ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+                ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+                [".", "9", "8", ".", ".", ".", ".", "6", "."],
+                ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+                ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+                ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+                [".", "6", ".", ".", ".", ".", "2", "8", "."],
+                [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+                [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+            ]
+        )
+        is True
+    )
